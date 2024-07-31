6e9001eefa903d9df86741b2cd7cb4db75e67288
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-07-31 15:32:08 +0900

    Title: 작은 구슬의 이동, Time: 120ms, Memory: 73MB, Status: Passed - CodeTree

--- 240731/작은 구슬의 이동/small-marble-movement.py
+++ 240731/작은 구슬의 이동/small-marble-movement.py
@@ -1,4 +1,4 @@
-n, t = map(int,input().split())
+n, t = tuple(map(int,input().split()))
 r,c,d = tuple(input().split())
 
 dir_x,dir_y = [1,0,-1,0],[0,1,0,-1]
@@ -17,7 +17,7 @@ ans_x,ans_y,move = int(r)-1,int(c)-1,dict_dir[d]
 for _ in range(t):
     nx,ny = ans_x + dir_x[move],ans_y+dir_y[move]
     if in_range(nx,ny):
-        x,y = nx,ny
+        ans_x,ans_y = nx,ny
     else:
         move = (move+2) % 4
 
