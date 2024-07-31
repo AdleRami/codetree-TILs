334b699662dec1a34e77413d4f771ee21078de70
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-07-31 14:42:50 +0900

    Title: 1이 3개 이상 있는 위치, Time: 156ms, Memory: 26MB, Status: Passed - CodeTree

--- 240731/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 240731/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -1,23 +1,27 @@
 n = int(input())
-arr = [list(map(int, input().split()) for _ in range(n))]
-dir_x, dir_y = [0,1,0,-1],[1,0,-1,0]
-cnt = 0
+arr = [list(map(int, input().split())) for _ in range(n)]
+
+dxs = [0, 1,  0, -1]
+dys = [1, 0, -1,  0]
 
 def in_range(x,y):
-    return 0<=x<n and 0<=y<n
+    return 0 <= x and x < n and 0 <= y and y < n
 
-def adjacent_cnt(x,y):
+def adjacent_cnt(x, y):
     cnt = 0
-    for dx,dy in zip(dir_x,dir_y):
-        nx,ny = x + dx, y + dy
-        if in_range(nx,ny) and arr[nx,ny] == 1:
-            cnt+=1
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if not in_range(nx, ny):
+            continue
+        if arr[nx][ny] ==1:
+            cnt += 1
+    
     return cnt
 
 ans = 0
 for i in range(n):
     for j in range(n):
-        if adjacent_cnt(i,j) >=3:
-            ans+=1
-
+        if adjacent_cnt(i, j) >= 3:
+            ans += 1
+    
 print(ans)
