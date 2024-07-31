d853e94d597339eb9aecaca1a1ee4e9c37e297bd
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-07-31 14:31:33 +0900

    Title: 1이 3개 이상 있는 위치, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240731/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,23 @@
+n = int(input())
+arr = [list(map(int, input().split()) for _ in range(n))]
+dir_x, dir_y = [0,1,0,-1],[1,0,-1,0]
+cnt = 0
+
+def in_range(x,y):
+    return 0<=x<n and 0<=y<n
+
+def adjacent_cnt(x,y):
+    cnt = 0
+    for dx,dy in zip(dir_x,dir_y):
+        nx,ny = x + dx, y + dy
+        if in_range(nx,ny) and arr[nx,ny] == 1:
+            cnt+=1
+    return cnt
+
+ans = 0
+for i in range(n):
+    for j in range(n):
+        if adjacent_cnt >=3:
+            ans+=1
+
+print(ans)
