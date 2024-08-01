8c91b54a484266d11233080fb98f7a54b4c9ea3a
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-01 15:58:33 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 134ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240801/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 240801/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -9,15 +9,14 @@ dx,dy = [0,1,0,-1],[1,0,-1,0]
 def in_range(x,y):
     return 0<=x<n and 0<=y<m
 x,y = 0,0
-for i in range(ord('B'), ord('B') + n*m-1):
+for i in range(ord('B')-ord('A'), ord('B')-ord('A') + n*m-1):
     nx,ny = x + dx[dir_num],y +dy[dir_num]
-    if i > ord('Z'):
-        i -= 25
+    order = i % 25
     if not in_range(nx,ny) or arr[nx][ny] != 0:
         dir_num = (dir_num + 1) % 4
     
     x,y = x + dx[dir_num], y + dy[dir_num]
-    arr[x][y] = chr(i)
+    arr[x][y] = chr(order+ord('A'))
 
 for i in range(n):
     for j in range(m):
