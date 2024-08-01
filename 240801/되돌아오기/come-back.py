39f576a4763cb0e6d25bbd77157706b578584198
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-01 12:16:59 +0900

    Title: 되돌아오기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240801/되돌아오기/come-back.py
@@ -0,0 +1,24 @@
+n = int(input())
+move = [input().split() for _ in range(n)]
+
+dx,dy = [1,0,-1,0],[0,-1,0,1]
+dict_dir={
+    'N': 0,
+    'W': 1,
+    'S': 2,
+    'E': 3
+}
+nx,ny = 0,0
+cnt = 0
+
+for i in move:
+    for j in range(int(i[1])):
+        nx += dx[dict_dir[i[0]]]
+        ny += dy[dict_dir[i[0]]]
+        cnt += 1
+        if nx==0 and ny ==0:
+            print(cnt)
+            exit()
+            
+if nx==0 and ny ==0:
+    print(-1)
