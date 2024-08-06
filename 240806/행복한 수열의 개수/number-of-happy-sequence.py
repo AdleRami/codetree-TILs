0c8047db283f36aab6ea57f3bb60992d1035d590
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-06 16:56:47 +0900

    Title: 행복한 수열의 개수, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

--- 240806/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240806/행복한 수열의 개수/number-of-happy-sequence.py
@@ -4,33 +4,36 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 def row_happy(x):
     check_happy_num = arr[x][0]
     
+    max_cnt = 0
     cnt = 0
     for i in range(1,n):
-        
         if check_happy_num == arr[x][i]:
-            cnt += 1
+            cnt += 1            
+            max_cnt = max(max_cnt,cnt)
         else:
-            check_happy_num = arr[x][i]
+            check_happy_num = arr[x][i]            
+            max_cnt = max(max_cnt,cnt)
             cnt = 0
-        
-        
     
-    return cnt
+    return max_cnt
 
 def cul_happy(x):
     check_happy_num = arr[0][x]
 
+    max_cnt = 0
     cnt = 0
     for i in range(1,n):
+        
         if check_happy_num == arr[i][x]:
             cnt += 1
+            max_cnt = max(max_cnt,cnt)
         else:
             check_happy_num = arr[i][x]
+            max_cnt = max(max_cnt,cnt)
             cnt = 0
         
-        #print(x,check_happy_num, cnt)
     
-    return cnt
+    return max_cnt
 
 num_happy = 0
 for i in range(n):
@@ -39,6 +42,6 @@ for i in range(n):
     if row_happy_num >= m-1:
         num_happy += 1
     if cul_happy_num >= m-1:
-        num_happy += 1   
+        num_happy += 1  
 
 print(num_happy)
