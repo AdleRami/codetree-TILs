f33b835b546986093258974c234f3d8495b74bbf
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-06 13:14:32 +0900

    Title: 총점 비교, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/총점 비교/compare-total-points.py
@@ -0,0 +1,22 @@
+#객체 선언
+class Student:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+#변수 입력
+n = int(input())
+
+students = []
+for _ in range(n):
+    name, kor, eng, math = tuple(input().split())
+    students.append(Student(name, int(kor), int(eng), int(math)))
+
+#총점 순으로 정렬
+students.sort(key = lambda x: x.kor + x.eng + x.math)
+
+#출력
+for i in range(n):
+    print(students[i].name, students[i].kor, students[i].eng, students[i].math)
