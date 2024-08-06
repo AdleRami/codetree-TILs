dd79d7e948ddf00810c415c3849ab70274f9132e
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-06 10:35:47 +0900

    Title: 오름 내림차순 정렬, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240806/오름 내림차순 정렬/inc-dec-sorting.py
@@ -0,0 +1,11 @@
+#변수 입력
+n = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+for i in arr:
+    print(i, end= ' ')
+print()
+arr.sort(reverse = True)
+for i in arr:
+    print(i, end = ' ')
