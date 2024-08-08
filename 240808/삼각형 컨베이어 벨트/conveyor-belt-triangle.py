9c01abd36847f31ef4b31f5b09f25186ab0622b3
AdleRami <73281078+AdleRami@users.noreply.github.com>
2024-08-08 23:40:20 +0900

    Title: 삼각형 컨베이어 벨트, Time: 222ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240808/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,33 @@
+n, t = map(int, input().split())
+belt = [list(map(int, input().split())) for _ in range(3)]
+
+for second in range(t):
+
+    #첫번째 줄에서 n번째 숫자 저장
+    belt_temp1 = belt[0][n-1]
+    #두번째 줄에서 n번째 숫자 저장
+    belt_temp2 = belt[1][n-1]
+    #세번째 줄에서 n번째 숫자 저장
+    belt_temp3 = belt[2][n-1]
+
+    for i in range(n-1, 0, -1):
+        #첫번째 줄 밀기
+        belt[0][i] = belt[0][i-1]
+
+        #2번째 줄 밀기
+        belt[1][i] = belt[1][i-1]
+
+        #3번째 줄 밀기
+        belt[2][i] = belt[2][i-1]
+
+    belt[0][0] = belt_temp3
+    belt[1][0] = belt_temp1
+    belt[2][0] = belt_temp2
+    
+
+
+#belt 출력
+for i in range(3):
+    for j in range(n):
+        print(belt[i][j], end = ' ')
+    print()
